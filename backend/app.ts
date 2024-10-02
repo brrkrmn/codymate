b0b63e8370abb193e68044cb72931e189127e5db
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-10-02 14:49:11 +0300

    configure mongodb

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,22 @@
 import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
+import { MONGODB_URI } from './utils/config';
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+console.log('Connecting to MONGODB...');
+
+mongoose
+  .connect(MONGODB_URI as string)
+  .then((result) => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB: ', error.message);
+  });
 
 app.use(express.json());
 app.use(cors());
