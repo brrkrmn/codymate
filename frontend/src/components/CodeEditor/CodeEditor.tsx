0518020c3f4d0ac6a44b7493f12881445d795db8
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-10-09 00:03:05 +0300

    auto focus created editors

--- frontend/src/components/CodeEditor/CodeEditor.tsx
+++ frontend/src/components/CodeEditor/CodeEditor.tsx
@@ -3,13 +3,14 @@ import { Theme } from "@/context/editor/editorProvider.types";
 import { useSceneContext } from "@/context/scene";
 import { Scene } from "@/context/scene/sceneProvider.types";
 import * as themes from "@uiw/codemirror-themes-all";
-import CodeMirror from "@uiw/react-codemirror";
-import { useCallback, useEffect, useState } from "react";
+import CodeMirror, { EditorView } from "@uiw/react-codemirror";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const CodeEditor = ({ scene }: { scene: Scene }) => {
   const { editScene, scenes } = useSceneContext();
   const [value, setValue] = useState(scene?.content || scenes[0].content);
   const { theme, extensions } = useEditorContext();
+  const editorRef = useRef<EditorView | null>(null);
 
   useEffect(() => {
     setValue(scene.content);
@@ -19,11 +20,24 @@ const CodeEditor = ({ scene }: { scene: Scene }) => {
     editScene({ ...scene, content: val });
   }, []);
 
+  const onCreateEditor = (editorView: EditorView) => {
+    editorRef.current = editorView;
+    if (editorRef.current) {
+      const endPos = editorRef.current.state.doc.length;
+      editorRef.current.dispatch({
+        selection: { anchor: endPos },
+      });
+      editorRef.current.focus();
+    }
+  };
+
   return (
     <div className={`w-full h-full p-10`}>
       <CodeMirror
         value={value}
         onChange={onChange}
+        autoFocus={true}
+        onCreateEditor={onCreateEditor}
         placeholder={
           scene?.number === 0
             ? "This will be the starting point of the video. Whatever written in this editor will not be animated. If you want to animate everything, create the first scene."
