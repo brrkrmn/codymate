e882ce6102a322a016c4c04892a2387a039748a9
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-10-14 08:37:33 +0300

    add google oauth

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,5 +1,18 @@
+import { signIn } from "@/auth";
+
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit">Signin with Google</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
