dcbf0d5c0a85249ff29e89a2a25ce024e9d4aeba
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-10-14 09:03:10 +0300

    add github oauth

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -11,6 +11,14 @@ const Login = () => {
       >
         <button type="submit">Signin with Google</button>
       </form>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github");
+        }}
+      >
+        <button type="submit">Signin with GitHub</button>
+      </form>
     </div>
   );
 };
