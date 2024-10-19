efea677783b1de9ea62f24504f9849dc50fadfdb
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-10-19 15:43:02 +0300

    refactor navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,66 +1,51 @@
 "use client";
 
-import { useSnippetContext } from "@/context/snippet";
-import { buttonStyles } from "@/styles";
+import { buttonStyles, buttonTextStyles } from "@/styles";
 import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
   Navbar as NextNavBar,
+  Tooltip,
 } from "@nextui-org/react";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
+import { LuLogOut } from "react-icons/lu";
 import Logo from "../../../public/logo.svg";
-import AuthButton from "./components/AuthButton/AuthButton";
 
 const Navbar = () => {
-  const { createSnippet } = useSnippetContext();
   const { status } = useSession();
-  const router = useRouter();
 
-  const handleOpenEditor = () => {
-    const id = uuidv4();
-    router.push(`/editor/${id}`);
-    createSnippet(id);
-  };
-
-  if (status === "loading") return null;
-  if (status === "unauthenticated") return null;
+  if (status === "loading" || status === "unauthenticated") return null;
   if (status === "authenticated") {
     return (
-      <div className="w-full flex items-center mt-4 px-4 sticky top-4 inset-x-0">
+      <div className="w-full flex items-center sticky top-0">
         <NextNavBar
-          maxWidth="lg"
+          maxWidth="full"
           height="40px"
-          isBordered
           classNames={{
-            base: "!mx-auto rounded-full w-[1024px] border-divider border-small backdrop-blur-sm",
-            wrapper: "rounded-full bg-content2 px-2 ",
+            base: "rounded-full backdrop-blur-sm py-4",
           }}
         >
           <NavbarBrand>
-            <Link href="/" className="ml-2">
+            <Link href="/">
               <Image src={Logo} alt="Logo" width={20} height={20} />
             </Link>
           </NavbarBrand>
           <NavbarContent className="ml-auto max-w-fit">
             <NavbarItem>
-              <button onClick={handleOpenEditor} className={`${buttonStyles}`}>
-                Playground
-              </button>
-            </NavbarItem>
-            <NavbarItem>
-              <AuthButton />
+              <Tooltip content="Logout" closeDelay={0} delay={200}>
+                <button className={buttonStyles} onClick={() => signOut()}>
+                  <LuLogOut className={buttonTextStyles} />
+                </button>
+              </Tooltip>
             </NavbarItem>
           </NavbarContent>
         </NextNavBar>
       </div>
     );
   }
-
 };
 
 export default Navbar;
