f4d1e030f975ae9a61fa9e6c28b9240669688445
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-10-25 14:38:56 +0300

    remove middleware

--- frontend/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { auth } from "./auth";
-
-export const middleware = async (request: NextRequest) => {
-  const session = await auth();
-  const path = request.nextUrl.pathname;
-
-  return NextResponse.next();
-};
